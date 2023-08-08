62bd7a9dbde8fd2e99adcb53ef5945c2e8af3929
Jal Thakor <jalsthakor413@gmail.com>
2023-08-08 18:27:44 +0530

    added user

---
+++ server/src/models/index.js
@@ -0,0 +1,11 @@
+const User=require("./user");
+// const UserToken=require("./userToken");
+// const Problem=require("./problem");
+// const TestCase=require("./testCase");
+// const Editorial=require("./editorial");
+// const ProblemSetter=require("./problemSetter");
+
+
+module.exports={
+    User
+}
